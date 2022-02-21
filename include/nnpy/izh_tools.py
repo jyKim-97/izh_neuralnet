5ef9b0053682ba210088d9bc6a810d3d66174cbf
jyKim-97 <rlawjddud73@khu.ac.kr>
2022-02-21 16:37:58 +0900

    udpate_220221

--- include/nnpy/izh_tools.py
+++ include/nnpy/izh_tools.py
@@ -56,7 +56,7 @@ def read_byte_data(fname, N):
     with open(fname, "rb") as fid:
         data = np.fromfile(fid, dtype=np.dtype(np.double))
     nline = len(data) // N
-    return data.reshape([nline, N], order='C').T
+    return data.reshape([N, nline], order='C')
 
 
 def get_phase(t_spks, N, ts):
@@ -84,3 +84,34 @@ def get_raster_plot(t_spks, cell_types):
     
     return ts, id_spks, cs
 
+
+def draw_raster_plot(tspk, xlim=None, ylim=None, colors=None, cell_types=None, s=1):
+    if colors is None:
+        colors = np.array([[203, 67, 53], [36, 113, 163]])/255
+    
+    num_cells = len(tspk)
+    if cell_types is None:
+        cell_types = np.ones(num_cells, dtype=np.int)
+    
+    # get scatter array
+    x, y, c = [], [], []    
+    for n in range(num_cells):
+        num_fire = len(tspk[n])
+        
+        x.extend(tspk[n])
+        y.extend(n * np.ones(num_fire))
+        c.extend(np.tile(colors[cell_types[n]], [num_fire, 1]))
+            
+    c = np.array(c)
+        
+    plt.scatter(x, y, s=s, c=c)
+    if xlim is not None:
+        plt.xlim(xlim)
+    if ylim is None:
+        ylim = [0, num_cells]
+    plt.ylim(ylim)
+    plt.xlabel('time (ms)', fontsize=12)
+    yt = [0, ylim[1]//2, ylim[1]]
+    plt.yticks(yt, labels=['#%d'%(i) for i in yt])
+    
+    return x, y, c
