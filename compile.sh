7abede51a3d12eb298d6da6628044673989b4666
jyKim-97 <rlawjddud73@khu.ac.kr>
2022-04-05 15:55:54 +0900

    updated compile bash

--- compile.sh
+++ compile.sh
@@ -4,5 +4,5 @@ CFLAGS="gcc"
 OPT="-g -O3 -Wall -std=c11"
 LDFLAGS="-lizh_neuralnet -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm"
 
-echo "$1 -> $2"
-$CFLAGS $OPT -o $2 $1 subfunction.o $INC $LIB $LDFLAGS
+echo "compile run simulation.c"
+$CFLAGS $OPT -o run_simulation.out run_simulation.c $INC $LIB $LDFLAGS
