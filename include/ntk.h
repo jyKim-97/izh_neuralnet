f7b9873e3053a4f35f8e04076da5c654ae02c2a4
jyKim-97 <rlawjddud73@khu.ac.kr>
2022-04-19 02:48:57 +0900

    replace argument in fixed in-degree network generation function

--- include/ntk.h
+++ include/ntk.h
@@ -19,10 +19,10 @@ void init_bi_ntk(int num_pre, int num_post, ntk_t *ntk);
 void gen_bi_random_ntk_mean_deg(int *pre_node_types, int *post_node_types, 
                             int mean_out_deg[MAX_TYPE][MAX_TYPE],
                             double str_cnt_type[MAX_TYPE][MAX_TYPE], ntk_t *ntk);
-void gen_bi_random_ntk_with_type(int *pre_node_types, int *post_node_types,
-                            double p_cnt_type[MAX_TYPE][MAX_TYPE],
-                            double str_cnt_type[MAX_TYPE][MAX_TYPE], ntk_t *ntk);
 void gen_bi_random_ntk_fixed_indeg(int *pre_node_types, int *post_node_types,
+                            int mean_out_deg[MAX_TYPE][MAX_TYPE],
+                            double str_cnt_type[MAX_TYPE][MAX_TYPE], ntk_t *ntk);
+void gen_bi_random_ntk_with_type(int *pre_node_types, int *post_node_types,
                             double p_cnt_type[MAX_TYPE][MAX_TYPE],
                             double str_cnt_type[MAX_TYPE][MAX_TYPE], ntk_t *ntk);
 void free_bi_ntk(ntk_t *ntk);
